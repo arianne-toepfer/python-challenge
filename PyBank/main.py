386a2e8c70e570a5ef07e72692a1767381e8eb07
arianne-toepfer <78578889+arianne-toepfer@users.noreply.github.com>
2021-04-02 22:42:30 -0700

    updates

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,34 +1,49 @@
 import os
 import csv
 
+#Create funciton for sum of profit loss
+def summation(List_of_num):
+    return sum(List_of_num)
+
+
 csvpath= r'C:\Users\Arianne\Desktop\PythonStuff\Python_Challenge_Homework\python-challenge\PyBank\resources\budget_data.csv'
 #Read function in CSV file:
 with open(csvpath) as csvfile:
     csvreader=csv.reader(csvfile, delimiter=",")
     header=next(csvreader)
+    
+    Total_Months = 0
+    maximum_profit= 0
+    minimum_profit= 0
+    Dictionary={"Date":[],"Profit_Loss":[]}
     for row in csvreader:
-        Date=str(row[0])
-        Profit_loss=int(row[1])
-
+        
         #Total month
-        Total_Months=len(Date)
+        Total_Months += 1
+
+        #Create Date list and Profit loss lists
+        Dictionary["Date"].append(row[0])
+        Dictionary["Profit_Loss"].append(int(row[1]))
 
         #total Profit loss
-        Total=sum(Profit_loss)
+        Total=summation(Dictionary["Profit_Loss"])
 
-        #Average Profit loss change
-        Average_profit = Total/Total_Months
+        #Average Profit Loss
+        Average = Total_Months / Total
 
-        #greatest increase in profit data and amount
-        max_profit=max(int(bank[1]))
-
-        #greatest decrease in loss date and amounte
-        min_profit= min(int(bank[1]))
-    
-        #print it pretty
-        print("Financial Analysis")
-        print("Total Months: " + Total_Months)
-        print("Total: " + Total)
-        print("Average Change: " + Average_profit)
-        print("Greatest Increase in Profits: " + max_profit)
-        print("Greatest Decrease in Profits: " + min_profit)
+        #Find max and min
+        if maximum_profit <= int(row[1]):
+            maximum_profit=  int(row[1])
+        if minimum_profit >= int(row[1]):
+            minimum_profit= int(row[1])
+        
+    #match up max and min with appropriate date they are locations 25 for max and 44 for min
+    #print(Dictionary["Profit_Loss"].index(maximum_profit)) 
+    #print(Dictionary["Profit_Loss"].index(minimum_profit)) 
+    #Print the summary
+    print("Financial Analysis")
+    print(f"Total Months: {Total_Months}")
+    print(f"Total Profits: {Total}")
+    print(f"Average Change: {Average}")
+    print("Greatest Increase in Profits: " + Dictionary["Date"].pop(25), Dictionary["Profit_Loss"].pop(25))
+    print("Greatest Decrease in Profits: " + Dictionary["Date"].pop(44), Dictionary["Profit_Loss"].pop(44))
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,32 +3,28 @@ import csv
 csvpath= r'C:\Users\Arianne\Desktop\PythonStuff\Python_Challenge_Homework\python-challenge\PyPoll\Resources\election_data .csv'
 
 #def election_results(poll):
-    #total Votes
-
-    #dictionary of candidates,the percentage of votes they won, and the total number of votes they won
-
-    #percentage of votes
-  
-    #find the winner of the election
-
-    #print it pretty
-
-
+#total Votes
+#create list of candidates,the percentage of votes they won, and the total number of votes they won
+#percentage of votes
+#find the winner of the election
+#print it pretty
 
 #Read function in CSV file:
 with open(csvpath) as csvfile:
     csvreader=csv.reader(csvfile, delimiter=",")
     header=next(csvreader)
+    votes=0
+    count=1
     for row in csvreader:
-       #total votes
-        Total= len(row[0])
-        print(Total)
-       #finding candidates
-        count=1
+        #total votes
+        votes += 1
+
+        #finding candidates
         candidates = []
-        for x in range(row[1]):
+        for x in range(int(row[1])):
             if x not in candidates:
                 candidates.append(x)
                 count+=1
-        print(candidates)
-        
+    
+    print(votes)
+    print(candidates)
